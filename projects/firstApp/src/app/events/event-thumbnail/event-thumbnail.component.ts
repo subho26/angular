e9907cea09f680e6f698c6520f6f602cc48c8199
Shubhendu Pramanik <subho.pramanik1@gmail.com>
2019-09-19 09:12:44 +0530

    added other component's public variable/ method acces process

--- projects/firstApp/src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ projects/firstApp/src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class EventThumbnailComponent implements OnInit {
 
   @Input() event: any;
+  thumbVar: String = 'Hello';
   constructor() { }
 
   ngOnInit() {
