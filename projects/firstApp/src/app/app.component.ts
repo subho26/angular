912b974b57551e3f15d34da3dec3cf5121767734
Shubhendu Pramanik <subho.pramanik1@gmail.com>
2019-09-20 07:44:13 +0530

    added nav bar component

--- projects/firstApp/src/app/app.component.ts
+++ projects/firstApp/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <h1>Hello Angular</h1>
+  <app-navbar></app-navbar>
   <app-events-list></app-events-list>
   `,
   styleUrls: ['./app.component.css']
